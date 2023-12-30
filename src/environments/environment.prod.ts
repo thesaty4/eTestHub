6ad13460845a76053fd86d9ee5073926988d756f
Satya Mishra <satya.mishra@bigohtech.com>
2023-12-30 14:44:34 +0530

    asset & environment setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  base_url: 'https://production-api-url',
+  // other production environment variables
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  base_url: 'http://localhost:4200',
+  // other development environment variables
+};
